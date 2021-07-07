42491e648148c32fb92280043c895274619810f6
Dung.HuynhTri <dung.huynhtri@petronas.com>
2021-07-07 10:28:55 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.scss";
+import picture1 from "./assets/images/image-header-desktop.jpg";
+import picture2 from "./assets/images/image-header-mobile.jpg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card-container">
+      <div className="card-gap"></div>
+      <div className="card-layout">
+        <div className="text-layout">
+          <div id="fInter700">
+            Get <span id="violet">insights</span> that help your business grow.
+          </div>
+          <div className="mt-3 paragraph">
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </div>
+          <div className="stats-layout">
+            <div style={{ flex: 1 }}>
+              <div id="stats">10k+</div>
+              <div id="stats-desc">COMPANIES</div>
+            </div>
+            <div style={{ flex: 1 }}>
+              <div id="stats">314</div>
+              <div id="stats-desc">TEMPLATES</div>
+            </div>
+            <div style={{ flex: 1 }}>
+              <div id="stats">12M+</div>
+              <div id="stats-desc">QUERIES</div>
+            </div>
+          </div>
+        </div>
+        <div className="image-layout">
+          <img src="" alt="picture" />
+        </div>
+      </div>
+      <div className="card-gap"></div>
     </div>
   );
 }
