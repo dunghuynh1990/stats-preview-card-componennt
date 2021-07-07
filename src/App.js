870f8934666999ad342d689d91b21c7a3c70f72c
Bryant Huynh <dunghuynh1990@gmail.com>
2021-07-07 16:06:49 +0800

    optimize css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <div className="card-gap"></div>
       <div className="card-layout">
         <div className="text-layout">
-          <div id="fInter700">
+          <div className="fInter700">
             Get <span id="violet">insights</span> that help your business grow.
           </div>
           <div className="mt-3 paragraph">
@@ -15,16 +15,16 @@ function App() {
           </div>
           <div className="stats-layout">
             <div style={{ flex: 1 }}>
-              <div id="stats">10k+</div>
-              <div id="stats-desc">COMPANIES</div>
+              <div className="stats">10k+</div>
+              <div className="stats-desc">COMPANIES</div>
             </div>
             <div style={{ flex: 1 }}>
-              <div id="stats">314</div>
-              <div id="stats-desc">TEMPLATES</div>
+              <div className="stats">314</div>
+              <div className="stats-desc">TEMPLATES</div>
             </div>
             <div style={{ flex: 1 }}>
-              <div id="stats">12M+</div>
-              <div id="stats-desc">QUERIES</div>
+              <div className="stats">12M+</div>
+              <div className="stats-desc">QUERIES</div>
             </div>
           </div>
         </div>
